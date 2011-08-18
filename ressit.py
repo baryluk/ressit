ed224abba768a9a591bb3afaca546070b6767eb2
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2011-08-18 23:34:43 +0200

    Move to the configuration file configuration. Support multiple reddits and multiple feeds in each reddit. Support dry_run.

--- ressit.py
+++ ressit.py
@@ -2,62 +2,111 @@
 
 import reddit as reddit
 import feedparser
-import time
+import time, sys
 import shelve
+import ConfigParser
 
-# Configuration section.
-feed_url         = "http://example.com/rss.xml"
-reddit_user      = "login"
-reddit_password  = "secret"
-reddit_subreddit = "name-of-subreddit"
+if len(sys.argv) != 2:
+	raise Exception("Usage: %s config_file.ini" % sys.argv[0])
 
-raise Exception("Please configure ressit first") # REMOVE, after configuring above ones.
+defaults = {
+	'submit_delay'     : 60,
+	'looping'          : True,   # daemon mode
+	'refresh_time'     : 3600,   # delay beeten loops
+	'retries'          : 2,      # number of feed download, and submission retries
+	'retry_delay'      : 60,     # delay beetwen retries
+	'error_delay'      : 600,    # how long wait after retries failed
+	'robust'           : True,   # if False, then errors will terminate script
+	'db_filename'      : "uploads.shelve.db",
+	'dry_run'          : False
+}
 
-# Please do not change, until You know what are you doing.
-refresh_time     = 3600
-submit_delay     = 60
+config_filename = sys.argv[1]
 
-while True:
-	s = shelve.open("uploads.shelve.db")
+config = ConfigParser.SafeConfigParser()
+config.add_section('general')
+for k, v in defaults.iteritems():
+	config.set('general', k, str(v))
+config.read([config_filename])
 
-	logged = False
+reddit_subreddits = []
+for section_name in config.sections():
+	if section_name != "general" and section_name.startswith("r/"):
+		reddit_subreddits.append(section_name)
+
+print "Found", len(reddit_subreddits), "subreddit sections."
+
+if len(reddit_subreddits) == 0:
+	raise Exception("No subreddits defined in configuration file")
+
+reddit_username  = config.get('general', 'password')
+reddit_password  = config.get('general', 'username')
+
+submit_delay     = config.getint('general', 'submit_delay')
+looping          = config.getboolean('general', 'looping')
+refresh_time     = config.getint('general', 'refresh_time')
+db_filename      = config.get('general', 'db_filename')
+dry_run          = config.getboolean('general', 'dry_run')
+
+while looping:
+	s = shelve.open(db_filename)
+
 	r = None
 	k = None
-	def login():
-		if logged:
-			return
-		logged = True
-		print "Logging in...",
-		r = reddit.Reddit(user_agent="ressit robot 0.1")
-		r.login(user=reddit_user, password=reddit_password)
-		print "logged,",
-		k = r.get_subreddit(reddit_subreddit)
-		print "got reddit."
 
-	# TODO: multifeed uploader
+	def login(r, k):
+		if r == None:
+			print "Logging in as %s ..." % (reddit_username),
+			r = reddit.Reddit(user_agent="ressit robot 0.1")
+			r.login(user=reddit_username, password=reddit_password)
+			print "logged,",
+		if k == None:
+			k = r.get_subreddit(reddit_subreddit)
+			print "got reddit."
+		return r, k
 
-	print time.asctime()
-	print "Retriving, parsing, and analyzing feed", feed_url
-	ignored, submited = 0, 0
-	for f in reversed(feedparser.parse(feed_url).entries):
-		key = str(f.link)
-		if key in s:
-			ignored += 1
+	for reddit_subreddit in reddit_subreddits:
+		section_name = reddit_subreddit
+		if reddit_subreddit.startswith("r/"):
+			reddit_subreddit = reddit_subreddit[2:]
+
+		k = None
+
+		print "Starting update for subreddit r/%s" % (reddit_subreddit)
+
+		feeds = config.items(section_name)
+		if len(feeds) == 0:
+			print "WARNING: No feeds for subreddit", reddit_subreddit
 			continue
-		login() # lazy login
-		print "submiting %d:" % (submited+1),
-		print f.title, f.link,
-		s[key] = 1
-		k.submit(url=f.link, title=f.title)
-		# TODO: automatically 'approve' this submissions
-		print "submited."
-		s[key] = 2 # TODO: date of upload
-		submited += 1
-		time.sleep(submit_delay)
+		# TODO: multifeed uploader
+		for key, feed_url in feeds:
+			print time.asctime()
+			print "Retriving, parsing, and analyzing feed", feed_url
+			ignored, submited = 0, 0
+			for f in reversed(feedparser.parse(feed_url).entries):
+				key = str(f.link)
+				if key in s:
+					ignored += 1
+					continue
+				r, k = login(r, k) # lazy login
+				print "submiting %d:" % (submited+1),
+				print f.title, f.link,
+				s[key] = 1
+				if not dry_run:
+					k.submit(url=f.link, title=f.title)
+				else:
+					print "dry run",
+				# TODO: automatically 'approve' this submissions
+				print "submited."
+				s[key] = 2 # TODO: date of upload
+				submited += 1
+				time.sleep(submit_delay)
 
-	print "Feed summary:", ignored, "ignored,", submited, "submited."
+			print "Feed summary:", ignored, "ignored,", submited, "submited."
+
 	s.close()
 	k = None
 	r = None
-	print "Sleeping", refresh_time, "second for feed updates."
-	time.sleep(refresh_time)
+	if looping:
+		print "Sleeping", refresh_time, "second for feed updates."
+		time.sleep(refresh_time)
